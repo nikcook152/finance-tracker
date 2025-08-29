b4e846d4327368952e2c7ee12a7a77127363f8d3
Niklas Koch <nik.cook@gmx.de>
2025-08-30 00:30:35 +0200

    fix(api): Add PrismaModule to AnalyticsModule

--- backend/src/analytics/analytics.module.ts
+++ backend/src/analytics/analytics.module.ts
@@ -1,9 +1,13 @@
+// backend/src/analytics/analytics.module.ts
+
 import { Module } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsController } from './analytics.controller';
+import { PrismaModule } from 'src/prisma/prisma.module'; // 1. Import this
 
 @Module({
+  imports: [PrismaModule], // 2. Add this line
   providers: [AnalyticsService],
-  controllers: [AnalyticsController]
+  controllers: [AnalyticsController],
 })
 export class AnalyticsModule {}
